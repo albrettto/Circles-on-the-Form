76e8101611792aa5a1b071915997580e4dd36476
khusamov Albert <khusamov01@mail.ru>
2020-11-17 22:26:12 +0500

    v1.0
    Написан класс CCircle

--- Form1.cs
+++ Form1.cs
@@ -10,6 +10,23 @@ using System.Windows.Forms;
 
 namespace Circles_on_the_Form
 {
+    public class CCircle
+    {
+        public int rad = 5;
+        public int x, y;
+        public CCircle()
+        {
+            x = 0 + rad;
+            y = 0 + rad;
+        }
+        public CCircle(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        ~CCircle() { }
+    }
+
     public partial class Form1 : Form
     {
         public Form1()
